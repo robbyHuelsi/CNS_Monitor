c3ac6e46b690c70e91da2240ddd8c50c22af2b7c
Robert Hülsmann <robert@huelsi.de>
2017-01-13 16:18:50 +0100

    mac adress test not done

--- src/cns_controller/NetworkMonitor.java
+++ src/cns_controller/NetworkMonitor.java
@@ -1,11 +1,17 @@
 package cns_controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Collections;
 import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import cns_main.CnsConfig;
 import config_utilities.Computer;
@@ -44,14 +50,7 @@ public class NetworkMonitor {
 		}
 		
 		for (InetAddress ipAdress : ipAdresses) {
-			try {
-				NetworkInterface network = NetworkInterface.getByInetAddress(ipAdress);
-				byte[] mac = network.getHardwareAddress();
-				System.out.print("MAC address for " + ipAdress.toString() + ": " + mac.toString());
-			} catch (SocketException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			System.out.println(getMac(ipAdress.toString().substring(1)));
 			
 		}
 		
@@ -59,7 +58,10 @@ public class NetworkMonitor {
 	
 	public boolean all_computers_reachable(){
 		//TODO check if all computer reachable
+		
 		getIpAdresses();
+
+		
 		Vector<Computer> all_computers = config.getAll_computers();
 		for (Computer computer : all_computers){
 			// TODO check availablity of computer
@@ -69,5 +71,81 @@ public class NetworkMonitor {
 		return true;
 	}
 	
+	
+	public String getMACAddress(String ip){ 
+        String str = ""; 
+        String macAddress = ""; 
+        try { 
+            Process p = Runtime.getRuntime().exec("nbtstat -A " + ip); 
+            InputStreamReader ir = new InputStreamReader(p.getInputStream()); 
+            LineNumberReader input = new LineNumberReader(ir); 
+            for (int i = 1; i <100; i++) { 
+                str = input.readLine(); 
+                if (str != null) { 
+                    if (str.indexOf("MAC Address") > 1) { 
+                        macAddress = str.substring(str.indexOf("MAC Address") + 14, str.length()); 
+                        break; 
+                    } 
+                } 
+            } 
+        } catch (IOException e) { 
+            e.printStackTrace(System.out); 
+        } 
+        return macAddress; 
+    }
+	
+	private String getMac(String ip) {
+		Pattern macpt = null;
+		
+	    // Find OS and set command according to OS
+	    String OS = System.getProperty("os.name").toLowerCase();
+
+	    String[] cmd;
+	    if (OS.contains("win")) {
+	        // Windows
+	        macpt = Pattern
+	                .compile("[0-9a-f]+-[0-9a-f]+-[0-9a-f]+-[0-9a-f]+-[0-9a-f]+-[0-9a-f]+");
+	        String[] a = { "arp", "-a", ip };
+	        cmd = a;
+	    } else {
+	        // Mac OS X, Linux
+	        macpt = Pattern
+	                .compile("[0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+");
+	        String[] a = { "arp", ip };
+	        cmd = a;
+	    }
+
+	    try {
+	        // Run command
+	        Process p = Runtime.getRuntime().exec(cmd);
+	        p.waitFor();
+	        // read output with BufferedReader
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(
+	                p.getInputStream()));
+	        String line = reader.readLine();
+
+	        // Loop trough lines
+	        while (line != null) {
+	            Matcher m = macpt.matcher(line);
+
+	            // when Matcher finds a Line then return it as result
+	            if (m.find()) {
+	                System.out.println("Found");
+	                System.out.println("MAC: " + m.group(0));
+	                return m.group(0);
+	            }
+
+	            line = reader.readLine();
+	        }
+
+	    } catch (IOException e1) {
+	        e1.printStackTrace();
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
+
+	    // Return empty string if no MAC is found
+	    return "";
+	}
 
 }
